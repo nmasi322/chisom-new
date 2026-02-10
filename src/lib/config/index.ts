602b7aa849b7e0a1c06b583d091ff87d1fd792b1
nmasi322 <edehdivine042@gmail.com>
2026-02-10 20:55:48 +0100

    chore: update tix link

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,3 +1,3 @@
 export const CONFIG = {
-    ticketsLink: {NG: "https://tix.africa/discover/thecreativewritingworkshop", UK: 'https://www.eventbrite.co.uk/e/the-creative-writing-workshop-tickets-1982501304770?aff=oddtdtcreator'}
+    ticketsLink: {NG: "https://tix.africa/discover/the-creative-writing-workshop-by-chisom-winifred", UK: 'https://www.eventbrite.co.uk/e/the-creative-writing-workshop-tickets-1982501304770?aff=oddtdtcreator'}
 }
